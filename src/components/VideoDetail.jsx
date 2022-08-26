97d9bf7667f5f349729393b1ebbba91541ef869d
Joeyg61703 <Joeyg61703@gmail.com>
2022-08-26 18:59:03 -0400

    Add Search Functionality

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, {useEffect, useState} from "react";
+import {Link, useParams} from "react-router-dom";
+import ReactPlayer from "react-player";
+import {Typography, Box, Stack} from "@mui/material";
+import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 
+import {Videos, Loader} from "./";
+import {fetchFromAPI} from "../utils/fetchFromAPI";
 const VideoDetail = () => {
-  return (
-    <div>VideoDetail</div>
-  )
-}
+  return <div>VideoDetail</div>;
+};
 
-export default VideoDetail
+export default VideoDetail;
